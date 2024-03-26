cd5a17057a2ea7b3f7b54d7d7bcf7c7ebd791fc8
Usuario <Usuario@CPD02-M15>
2024-03-26 11:02:02 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha entre par ou ímpar. Para ímpar digite 1 e para par digite 0");
+            int opcao = int.Parse(Console.ReadLine());
+
+            int count = opcao;
+
+            while (count <= 20) 
+            {
+               Console.WriteLine(count);
+                count += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
